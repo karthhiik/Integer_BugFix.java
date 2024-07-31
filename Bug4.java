52b08fe4bdbb4db735f7972f2cb2266481f10570
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 13:53:15 +0530

    Create Bug4.java

---
+++ Bug4.java
@@ -0,0 +1,11 @@
+public class Bug4{
+Scanner in=new Scanner(System.in);
+int temp=1;
+int a=in.nextInt();
+for(int i=1;i!=a;i--) {
+temp=temp*i;	
+  // Fix the Bugs and Issues
+}
+System.out.println("factorial:"+temp);
+}
+}
