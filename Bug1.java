1031f610108a18862bf87a21fe4dcaef2036a0e2
karthhiik <karthikthatikonda18@gmail.com>
2024-09-05 19:00:40 +0530

    Update Bug1.java

--- Bug1.java
+++ Bug1.java
@@ -1,19 +1,20 @@
+import java.util.*;
 public class Bug1{
 public static void main(String[] args) {
-	Scanner in=new Scanner(System.in);
-	System.out.println("Enter your Number");
-	
-	int a=in.nextInt();
-	for(int i=1;i<a;i++) {
-		if(a%i==0) {
-			a=a+i;
-			System.out.println(i);
-		}
-		
-	}
-	if(i==a) {
-		System.out.println("Perfect Number"+a);
-	}
-// Fix the Bug and Print Perfect Number or Not
+	Scanner in = new Scanner(System.in);
+        System.out.println("Enter your Number");
+        int sum = 0;
+        int a = in.nextInt();
+        for (int i = 1; i <= a / 2; i++) {
+            if (a % i == 0) {
+                sum = sum + i;
+            }
+
+        }
+        if (sum == a) {
+            System.out.println("Perfect Number:: " + a);
+        } else {
+            System.out.println("Not a Perfect Number:: " + a);
+        }
 }
 }
