9360bb7feb61faea3f86770ea6175bf1a8dc4409
karthhiik <karthikthatikonda18@gmail.com>
2024-09-05 19:03:32 +0530

    Update Bug4.java

--- Bug4.java
+++ Bug4.java
@@ -1,11 +1,13 @@
+import java.util.*;
 public class Bug4{
-Scanner in=new Scanner(System.in);
-int temp=1;
-int a=in.nextInt();
-for(int i=1;i!=a;i--) {
-temp=temp*i;	
-  // Fix the Bugs and Issues
-}
-System.out.println("factorial:"+temp);
-}
+public static void main(String[] args){
+   Scanner in = new Scanner(System.in);
+        int temp = 1;
+        int a = in.nextInt();
+        for (int i = 1; i <= a; i++) {
+            temp = temp * i;
+            // Fix the Bugs and Issues
+        }
+        System.out.println("factorial:" + temp);
+} 
 }
