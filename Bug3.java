48950804d8da231f6937d2142018af1f9fc91e4f
karthhiik <karthikthatikonda18@gmail.com>
2024-09-05 19:02:40 +0530

    Update Bug3.java

--- Bug3.java
+++ Bug3.java
@@ -1,21 +1,20 @@
+import java.util.*;
 public class Bug3{
 public static void main(String[] args) {
-	Scanner in=new Scanner(System.in);
-System.out.println("Enter");
-	int a=in.nextInt();
-	int temp=0;
-	int pal=a;
-while(a!=0) {
-	int rem=a/10;
-	temp=temp/10+rem;
-	a=a%10;
-}
-if(pal==temp) {
-System.out.println("Palindrome------"+temp);	
-}
-else 
-{
-System.out.println("Reverse----"+temp);
-	// Fix the Issue and Check Palindrome or not
-}	}
+	 Scanner in = new Scanner(System.in);
+        System.out.println("Enter");
+        int a = in.nextInt();
+        int pal = a;
+        int rev = 0, rem;
+        while (a != 0) {
+            rem = a % 10;
+            rev = rev * 10 + rem;
+            a = a / 10;
+        }
+        if (rev == pal) {
+            System.out.println("Number is Palindrome");
+        } else {
+            System.out.println("Number is not Palindrome");
+        }
+	}
 }
